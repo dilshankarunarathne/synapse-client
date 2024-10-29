d46476540992387479f40e23d717b4861ee6f0f7
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-10-30 03:50:59 +0530

    Update web_logger.py

--- logger/web_logger.py
+++ logger/web_logger.py
@@ -1,11 +1,29 @@
 import requests
+import json
+import os
 from config_parser.config import Config
 
 config = Config('cfg/client_config.cfg')
 LOG_SERVER_URL = config.get('Logging', 'LOG_SERVER_URL')
 
+DATA_FILE = 'client_data.json'
 
-def log(message):  # TODO add client id to log message
+
+def load_client_id():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            client_data = json.load(f)
+            return client_data.get('client_id')
+    return None
+
+
+def log(message):
+    client_id = load_client_id()
+    if client_id:
+        message = f"[Client-ID: {client_id}] {message}"
+    else:
+        message = "[Client-ID: Unregistered] " + message
+
     print(message)
     message = "[Client-Core] " + message
     try:
