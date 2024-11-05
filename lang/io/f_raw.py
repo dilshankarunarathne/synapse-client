d6e10025756f60aef269cb541753a9adc44e530e
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-05 09:31:03 +0530

    logging

--- lang/io/f_raw.py
+++ lang/io/f_raw.py
@@ -1,3 +1,6 @@
+from logger.web_logger import log
+
+
 def get_input_data(data_file_path: str, mode='int_sep_n') -> list:
     if mode == 'int_sep_n':
         return get_int_sep_n(data_file_path)
@@ -8,15 +11,13 @@ def get_input_data(data_file_path: str, mode='int_sep_n') -> list:
 
 def get_int_sep_n(file_path: str) -> list:
     # read data from file
-    data = read_file(data_file_path)
+    data = read_file(file_path)
 
     # convert data to a list of integers
     return list(map(int, data.split('\n')))
 
-    # return the list
-    return data
-
 
 def read_file(file_path):
+    log(f"[STDLib] Reading data from file: {file_path}")
     with open(file_path, 'r') as f:
         return f.read()
